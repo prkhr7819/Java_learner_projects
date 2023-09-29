5cf323646d29d9082d2959626d1e87ec55e62249
prkhr7819 <124690771+prkhr7819@users.noreply.github.com>
2023-09-29 12:33:57 +0530

    Update Number_guessing.java

--- proj.1/Number_guessing.java
+++ proj.1/Number_guessing.java
@@ -1,1 +1,37 @@
-king kong
+import java.util.*;
+import java.io.*;
+class Solution{
+    static int count=1;
+    public static void guess(int rand){
+        Scanner sc = new Scanner(System.in);
+        int num= sc.nextInt();
+        if(num==rand){
+            System.out.println("You found the number.");
+            System.out.print("The random number was ");
+            System.out.println(rand);
+            return;
+        }
+        else if (num<0) {
+            System.out.printf("Its ok to Give Up:\nComputer generated number was %d\n",rand);
+            return;
+        }
+        else if (num<rand) {
+            System.out.println("The desired number is greater than this.");
+        }
+        else if (num>rand) {
+            System.out.println("The desired number is smaller than this.");
+        }
+        count++;
+        guess(rand);
+    }
+    public static void main(String []argh)
+    {
+
+        System.out.println("--------- This is random value guesser --------\nRule-1) computer will take a value and user has to guess what the value is:");
+        System.out.println("Rule-2) Computer will tell if your number is smaller or greater than te computer generated number,");
+        System.out.println("Rule-3) Enter -1 to exit and find out what the number is.\n");
+        int rand=(int)(Math.random()*100);
+        guess(rand);
+        System.out.printf("You took %d tries\n",count);
+    }
+}

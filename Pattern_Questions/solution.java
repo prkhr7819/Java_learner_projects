fc8bf49508883339081f2d4b0a45b9ab27d9f42e
prkhr7819 <124690771+prkhr7819@users.noreply.github.com>
2023-11-07 23:20:48 +0530

    Update solution.java

--- Pattern_Questions/solution.java
+++ Pattern_Questions/solution.java
@@ -1,4 +1,18 @@
-//First Problem
+//Level Easy
+import java.util.*;
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+//Level Medium
 import java.util.*;
 class Solution{
     public static void main(String[] args) {
@@ -14,7 +28,7 @@ class Solution{
         }
     }
 }
-//Second Problem
+//Level Hard
 import java.util.*;
 class Solution{
     public static void main(String[] args) {

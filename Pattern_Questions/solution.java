c33a8e61f9779d11051ce3cebf61cddce82b6556
prkhr7819 <124690771+prkhr7819@users.noreply.github.com>
2023-11-07 23:17:10 +0530

    Create solution.java

---
+++ Pattern_Questions/solution.java
@@ -0,0 +1,40 @@
+//First Problem
+import java.util.*;
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            System.out.print("*");
+            for(int j=1;j<=i;j++){
+                System.out.print("-");
+            }
+            System.out.print("*");
+            System.out.print("\n");
+        }
+    }
+}
+//Second Problem
+import java.util.*;
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int j=0;j<num;j++){
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        for(int i=0;i<num;i++){
+            if(i<num-1){
+                System.out.print("*");
+            }
+            for(int j=num-1;j>i+2;j--){
+                System.out.print("-");
+            }
+            if(i<num-2){
+                System.out.print("*");
+                System.out.print("\n");
+            }
+        }
+    }
+}
